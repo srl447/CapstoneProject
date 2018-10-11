36d349f0f96c02629c8cdc8c3a8296371db4e585
Unknown <srl447@nyu.edu>
2018-10-11 16:41:29 -0400

    Starting work for mouse looking

--- Assets/Scripts/Player/LookAtMouse.cs
+++ Assets/Scripts/Player/LookAtMouse.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class LookAtMouse : MonoBehaviour {
 
+    public Transform cursor;
+    float xDir, yDir;
+
+    public Animator anim;
 	// Use this for initialization
 	void Start () {
 		
@@ -11,6 +15,26 @@ public class LookAtMouse : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
-	}
+
+        //dir = Mathf.Rad2Deg*Mathf.Tan((cursor.position.y - transform.position.y) / (cursor.position.x - transform.position.x));
+        //Debug.Log((cursor.position.y - transform.position.y) / (cursor.position.x - transform.position.x));
+        xDir = cursor.position.x - transform.position.x;
+        yDir = cursor.position.y - transform.position.y;
+        if (xDir >= 0 && Mathf.Abs(xDir) > Mathf.Abs(yDir))
+        {
+            Debug.Log("right");
+        }
+        if(xDir < 0 && Mathf.Abs(xDir) > Mathf.Abs(yDir))
+        {
+            Debug.Log("left");
+        }
+        if (yDir >= 0 && Mathf.Abs(yDir) > Mathf.Abs(xDir))
+        {
+            Debug.Log("up");
+        }
+        if (yDir < 0 && Mathf.Abs(yDir) > Mathf.Abs(xDir))
+        {
+            Debug.Log("down");
+        }
+    }
 }
