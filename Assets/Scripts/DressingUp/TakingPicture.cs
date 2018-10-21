8b728ac8edf53849b9b34a413fe1cf2501dfa625
Unknown <srl447@nyu.edu>
2018-10-21 19:20:11 -0400

    Start Screen

---
+++ Assets/Scripts/DressingUp/TakingPicture.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakingPicture : MonoBehaviour {
+
+    string screenShotName;
+    //string filepath = "/Capstone/Assets/Sprites/Screenshots";
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("works");
+            screenShotName = "finalPic" + Mathf.Floor(Random.Range(0, 9999999)) + ".png";
+            ScreenCapture.CaptureScreenshot(screenShotName);
+        }
+		
+	}
+}
