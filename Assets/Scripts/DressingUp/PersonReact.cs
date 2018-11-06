9f6c10d0f4b4b3ab40d51422e9a5b6dd1b0c4908
Unknown <srl447@nyu.edu>
2018-11-06 00:28:52 -0500

    some art?

---
+++ Assets/Scripts/DressingUp/PersonReact.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PersonReact : MonoBehaviour {
+
+    public Sprite sad, happy;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        Debug.Log("colliding");
+        if(collision.gameObject.tag == "Clothes" && (collision.gameObject.transform.localScale.x < 1 || collision.gameObject.transform.localScale.x > 1.3))
+        {
+            GetComponent<SpriteRenderer>().sprite = sad;
+        }
+        else
+        {
+            GetComponent<SpriteRenderer>().sprite = happy;
+        }
+
+    }
+}
