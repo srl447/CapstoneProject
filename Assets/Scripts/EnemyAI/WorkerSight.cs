a95bbcfe5efa1cf13425ac70589baeb8a0695c4c
Unknown <srl447@nyu.edu>
2018-12-02 20:27:15 -0500

    Worker Sight

---
+++ Assets/Scripts/EnemyAI/WorkerSight.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WorkerSight : MonoBehaviour {
+
+    public GameObject topUI;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            collision.gameObject.GetComponent<Movement>().up = false;
+            collision.gameObject.GetComponent<Movement>().left = false;
+            collision.gameObject.GetComponent<Movement>().down = false;
+            collision.gameObject.GetComponent<Movement>().right = false;
+            collision.gameObject.GetComponent<Movement>().enabled = false;
+            topUI.SetActive(true);
+        }
+    }
+}
--- Assets/Scripts/EnemyAI/Workers.cs
+++ Assets/Scripts/EnemyAI/Workers.cs
@@ -28,6 +28,5 @@ public class Workers : MonoBehaviour {
                 pointIndex = 0;
             }
         }
-        
 	}
 }
