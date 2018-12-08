d35f3042f094a5d1cc2275b908b22cf251a6b4a0
Unknown <srl447@nyu.edu>
2018-12-08 17:39:23 -0500

    Grabby Hand

--- Assets/Scripts/UI/CursorSprite.cs
+++ Assets/Scripts/UI/CursorSprite.cs
@@ -4,8 +4,9 @@ using UnityEngine;
 
 public class CursorSprite : MonoBehaviour {
 
-    public GameObject checkOut, clothes;
-    public Sprite cursor, hand;
+    public GameObject checkOut, clothes, circle;
+    public Sprite cursor, hand, grabbyHand;
+    bool col;
 	// Use this for initialization
 	void Start () {
         Cursor.visible = false;
@@ -15,11 +16,32 @@ public class CursorSprite : MonoBehaviour {
 	void Update () {
         if (checkOut.activeSelf || clothes.activeSelf)
         {
-            GetComponent<SpriteRenderer>().sprite = hand;
+            circle.GetComponent<SpriteRenderer>().enabled = false;
+            if (col)
+            {
+                GetComponent<SpriteRenderer>().sprite = grabbyHand;
+            }
+            else
+            {
+                GetComponent<SpriteRenderer>().sprite = hand;
+            }
         }
         else
         {
             GetComponent<SpriteRenderer>().sprite = cursor;
+            circle.GetComponent<SpriteRenderer>().enabled = True;
         }
 	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Clothes")
+        {
+            col = true;
+        }
+        else
+        {
+            col = false;
+        }
+    }
 }
