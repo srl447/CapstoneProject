9d0404f769495852a51a8a738f031b0999bada98
Unknown <srl447@nyu.edu>
2018-12-21 14:13:01 -0500

    No Shoplifting better

---
+++ Assets/Scripts/Player/Stealing.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Stealing : MonoBehaviour {
+
+    public GameObject wall;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player" && collision.gameObject.GetComponent<PickupClothes>().purchased == false && GameManager.clothes.Count > 0)
+        {
+            wall.SetActive(true);
+            collision.gameObject.GetComponent<ThoughtText>().add(new Node("Even if the register is scary, I can't shoplift!", 3f));
+        }
+        
+    }
+}
